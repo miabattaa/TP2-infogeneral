da4433f82d669be819eb54575329a4ba0d55fd27
Mia Battaglia <miabattaglia12@gmail.com>
2024-10-21 19:03:43 -0300

    desarrollo casi completo de las paginas y el estilo, falta realizar la pagina historia y el form de contacto, retocar estilos

---
+++ js/escuchar.js
@@ -0,0 +1,44 @@
+//redireccionar para escuchar las canciones
+    //self titled
+    let boton1 = document.querySelector("#top");
+
+boton1.addEventListener('click', () =>
+	location.href= 'https://www.youtube.com/watch?v=4-Q_0iKNnVM'
+	);
+    //regional at best
+    let boton2 = document.querySelector("#rab");
+
+boton2.addEventListener('click', () =>
+	location.href= ' https://www.youtube.com/watch?v=fm5kiVEklGA'
+	);
+   //vessel
+    let boton3 = document.querySelector("#vessel");
+
+boton3.addEventListener('click', () =>
+	location.href= 'https://www.youtube.com/watch?v=92XVwY54h5k'
+	);
+    //blurryface
+    let boton4 = document.querySelector("#blurryface");
+
+boton4.addEventListener('click', () =>
+	location.href= 'https://www.youtube.com/watch?v=92XVwY54h5k'
+	);
+    //trench
+    let boton5 = document.querySelector("#trench");
+
+boton5.addEventListener('click', () =>
+	location.href= 'https://www.youtube.com/watch?v=8mn-FFjIbo8'
+	);
+    //sai
+    let boton6 = document.querySelector("#sai");
+
+boton6.addEventListener('click', () =>
+	location.href= 'https://www.youtube.com/watch?v=6_GCsQRS3kM'
+	);
+     //clancy
+     let boton7 = document.querySelector("#clancy");
+
+ boton7.addEventListener('click', () =>
+    location.href= 'https://www.youtube.com/watch?v=AupwoN8QvbU'
+    );
+   
