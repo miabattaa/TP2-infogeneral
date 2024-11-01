578d9e0647602feb36d701058ed25cc1a6234074
Mia Battaglia <miabattaglia12@gmail.com>
2024-11-01 20:40:34 -0300

    pagina terminada

--- js/formulario.js
+++ js/formulario.js
@@ -24,16 +24,26 @@ function ingresar(){
         alert("Por favor, ingrese su opinion para ayudarnos a mejorar nuestra página");
         return false
     }
+    let email = document.querySelector("#mail").value; 
+    if (!validarEmail(email)) {
+        alert("Por favor, ingrese un correo electrónico válido");
+        return false}
     alert("Gracias " + nombre + " por visitar nuestro sitio!!!");
     return true;
 }
 
+function validarEmail(email) {
+    // valida que haya contenido antes y dsp del @ y un .com
+    const regex = /^[^@]+@[^@]+\.[a-z]{2,}$/i;
+    return regex.test(email);
+}
 //vaciar
 function vaciarDatos() {
     document.querySelector('#nombre').value = '';
     document.querySelector('#apellido').value = '';
     document.querySelector('#edad').value = '';
     document.querySelector("#comentarios").value = '';
+    document.querySelector("#mail").value = '';
 }
 
 //event
